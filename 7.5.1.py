cfbe5fec42690a896acee2311190e7bde04b3136
hasanbasuwala <hasanbasuwala50@gmail.com>
2026-07-23 15:26:56 +0530

    Update 7.5.1.py

--- 7.5.1.py
+++ 7.5.1.py
@@ -531,7 +531,6 @@ class DownloaderEngine:
                     url_lower = req.url.lower()
                     content_type = response.headers.get("content-type", "").lower()
                     
-                        # --- ADDED: Ignore background MP3 notification sounds ---
                     bad_keywords = ["google", "analytics", "ad", "beacon", "vast", "blank", "trailer", "promo", ".mp3", "audio"]
                     if any(bad in url_lower for bad in bad_keywords): return
                     
